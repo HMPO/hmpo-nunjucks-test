ebde6d2dc5144db103bb7710fbc0f011f88bd7aa
Paul Winkler <paul.winkler@digital.homeoffice.gov.uk>
2019-05-11 23:42:12 +0100

    Add tests

---
+++ test/index.js
@@ -0,0 +1,57 @@
+
+const path = require('path');
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+
+let nunjucksTest = require('../');
+
+describe('nunjucks render', () => {
+    let render;
+
+    beforeEach(() => {
+        render = nunjucksTest.renderer(
+            [
+                path.resolve(__dirname, 'views')
+            ],
+            [
+                path.resolve(__dirname, 'locale', 'locale1.json'),
+                path.resolve(__dirname, 'locale', 'locale2.json')
+            ]
+        );
+    });
+
+    it('renders a component', () => {
+        let $ = render({ component: 'testComponent', params: { a: 1, b: 2 }, ctx: true, ignore: true });
+        let html = nunjucksTest.cleanHtml($('body'));
+
+        expect(html).to.equal('<pre>{&quot;a&quot;:1,&quot;b&quot;:2}</pre><p>[test2]</p><p>[test3]</p>');
+    });
+
+    it('renders a template', () => {
+        let $ = render('test.html');
+        let html = nunjucksTest.cleanHtml($('body'));
+
+        expect(html).to.equal('<p>html [test1]</p>');
+    });
+
+    it('renders a string', () => {
+        let $ = render({ string: '<b>string {{translate("test2")}}</b>', translate: true });
+        let html = nunjucksTest.cleanHtml($('body'));
+
+        expect(html).to.equal('<b>string baz</b>');
+    });
+
+    it('renders a throws an error if translation not found', () => {
+        expect(() => {
+            render({ component: 'testComponent', ctx: true });
+        }).to.throw('Error: Translation not found for test3');
+    });
+
+    it('formats a string', () => {
+        let $ = render({ string: '<p><b>string {{translate("test2")}}</b></p>', translate: true });
+        let html = nunjucksTest.formatHtml($('body'));
+
+        expect(html).to.equal('<p><b>string baz</b>\n</p>');
+    });
+});
